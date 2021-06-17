1557cbd497938c66e1185e5592cc31653c0e2a84
csemanish12 <sahmanish20@gmail.com>
2021-06-17 23:41:10 +0545

    implement hyperlinked relationships

--- rest_sample/snippets/urls.py
+++ rest_sample/snippets/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import SnippetList, SnippetDetail, UserList, UserDetail
+from .views import SnippetList, SnippetDetail, UserList, UserDetail, api_root, SnippetHighlight
 
 urlpatterns = [
-    path('snippets/', SnippetList.as_view()),
-    path('snippets/<int:pk>/', SnippetDetail.as_view()),
-    path('users/', UserList.as_view()),
-    path('users/<int:pk>/', UserDetail.as_view()),
+    path('snippets/', SnippetList.as_view(), name='snippet-list'),
+    path('snippets/<int:pk>/', SnippetDetail.as_view(), name='snippet-detail'),
+    path('users/', UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
+    path('', api_root),
+    path('snippets/<int:pk>/highlight', SnippetHighlight.as_view(), name='snippet-highlight'),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
